ad91974d30616e508b57bffd43f646c4cd13970a
victormorello <90576035+victormorello@users.noreply.github.com>
2023-03-25 11:32:06 +0100

    Change sine animation and rename Sounds
    Added changing characters to sine animation, renamed Sounds to "Listen"

--- assets/js/sine.js
+++ assets/js/sine.js
@@ -7,6 +7,11 @@ let dx; // Value for incrementing x
 let yvalues; // Using an array to store height values for the wave
 let dotSize = 3;
 let maxHeight = 200;
+const charArray = ["I","⥜","⥠","⥔","⟷","⥐","⟷","⥊"];
+let currentChar = 0;
+let intervalID;
+
+
 
 function setup() {
   if (windowWidth < 600) {
@@ -20,6 +25,8 @@ function setup() {
   w = width + xspacing;
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w / xspacing));
+  setChar();
+  updateInterval(); // Call the updateInterval function once at the beginning
   
 }
 
@@ -30,6 +37,8 @@ function draw() {
   renderWave();
 }
 
+
+
 function calcWave() {
   // Increment theta (try different values for
   // 'angular velocity' here)
@@ -39,7 +48,6 @@ function calcWave() {
     theta +=0.12;
   }
   
-
   // For every x value, calculate a y value with sine function
   let x = theta;
   for (let i = 0; i < yvalues.length; i++) {
@@ -49,13 +57,29 @@ function calcWave() {
   
 }
 
-function renderWave() {
-  
+
+
+
+function renderWave() { 
   noStroke();
   fill(0, 85);
   // A simple way to draw the wave with an ellipse at each location
   for (let x = 0; x < yvalues.length; x++) {
-    ellipse(dotSize + x * xspacing, height / 2 + yvalues[x], dotSize, dotSize);
+    text(charArray[currentChar],dotSize + x * xspacing, height / 2 + yvalues[x])
+    //ellipse(dotSize + x * xspacing, height / 2 + yvalues[x], dotSize, dotSize);
   }
-
 }
+
+function setChar(){
+  currentChar = Math.floor(Math.random() * charArray.length);
+  console.log("Current Char:",currentChar);
+  updateInterval();
+}
+
+function updateInterval() {
+  let intervalTime = random(0.2, 1) * 1000; // Generate a random interval between 0.5 and 2 seconds
+  clearInterval(intervalID); // Clear the previous interval
+  intervalID = setInterval(setChar, intervalTime); // Set the new interval
+  console.log("Interval updated to:", intervalTime);
+}
+
